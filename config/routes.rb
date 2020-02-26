2e89829892ddd2690a4a6c03e95dc84ac7fda7a1
Victor Park <victor.shpark@gmail.com>
2020-02-26 16:14:21 -0300

    controllers and views created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+  get 'orders/create'
+  get 'orders/edit'
+  get 'orders/update'
+  get 'orders/destroy'
+  get 'users/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :instruments
+
+  resources :users, only: [ :show ] do
+    resources :instruments, only: [ :index ]
+  end
+
+  resources :orders, only: [ :new, :create, :edit, :update, :destroy]
+
 end
